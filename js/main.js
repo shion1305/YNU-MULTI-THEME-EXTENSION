bd112838354bbab83f657b3cd5ea25d255de9b59
arfes0e2b3c <webyata@gmail.com>
2022-01-23 00:20:42 +0900

    add main.js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+omit();
+function omit() {
+  const elements = document.getElementsByTagName("a");
+  for(const elt of elements) {
+    if (elt.innerText.includes("[")) {
+      elt.innerText = elt.innerText.split("[")[0];
+    }
+    if (elt.innerText.includes("【連絡専用】")) {
+      elt.innerText = elt.innerText.split("【連絡専用】")[1];
+    }
+  }
+}
